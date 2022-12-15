5e329e92d2db2d9da64e740f5ec70dfadc445e80
Finesse <hunchothafinesseboi@gmail.com>
2022-12-15 17:32:55 +0100

    Initial Commit

--- pay_js/register.js
+++ pay_js/register.js
@@ -43,7 +43,7 @@ $('#user').click(function() {
       message.style.color = 'red';
       return false;
   }else{
-      window.location.href = 'user.html'
+      window.location.href = '/Finesse/user.html'
   }
 })
 
